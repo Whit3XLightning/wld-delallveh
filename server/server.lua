876a536a673cf8776b0112c22e731314b4149420
Clayton Brown <45186932+Whit3XLightning@users.noreply.github.com>
2019-05-22 22:55:55 -0500

    Fixed config values

--- server/server.lua
+++ server/server.lua
@@ -3,4 +3,4 @@
 --https://github.com/Whit3XLightning--
 --------------------------------------
 
-RegisterCommand(Config.commandName, function(source, args, rawCommand) TriggerClientEvent("wld:delallveh", -1) end, false)
+RegisterCommand(Config.commandName, function(source, args, rawCommand) TriggerClientEvent("wld:delallveh", -1) end, Config.restricCommand)
