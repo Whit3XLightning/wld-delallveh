6c878ba950c7436a443a200ed2bf4df816890360
Clayton Brown <45186932+Whit3XLightning@users.noreply.github.com>
2020-08-16 15:09:06 -0500

    converted to fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+--------------------------------------
+------Created By Whit3Xlightning------
+--https://github.com/Whit3XLightning--
+--------------------------------------
+
+fx_version 'bodacious'
+game 'gta5'
+
+server_script {
+    'config.lua',
+    'server/server.lua'
+}
+client_scripts {
+    'config.lua',
+    'client/client.lua',
+    'client/entityiter.lua'
+}
+
