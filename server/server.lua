8b3621b0e9086341c30fa848151ee57d20b5d7af
sondremann2015 <68125004+sondremann2015@users.noreply.github.com>
2021-03-12 17:42:26 +0100

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,24 @@
---------------------------------------
-------Created By Whit3Xlightning------
---https://github.com/Whit3XLightning--
---------------------------------------
+function TimerDown (source)
+  local timeLeft = 60
+  
+  while timeLeft > 0 do
+      if timeLeft == 60 or timeLeft == 30 or timeLeft == 15 or timeLeft == 10 or timeLeft == 5 then
+        sendMsg(-1, Config.dvamsg1 .. GetPlayerName(source) .. Config.dvamsg2 .. timeLeft .. " Seconds")
+      end
+      
+      timeLeft = timeLeft - 1
+      Wait(1000)
+  end
+  
+  -- Gjer det du skal
+  sendMsg(-1, Config.dvcomp) 
+  TriggerClientEvent("wld:delallveh", -1)
+end
 
-RegisterCommand(Config.commandName, function(source, args, rawCommand) TriggerClientEvent("wld:delallveh", -1) end, Config.restricCommand)
+RegisterCommand(Config.commandName, function(source) 
+  TimerDown(source);
+end, true)
+
+function sendMsg(src, msg) 
+	TriggerClientEvent('chatMessage', -1, msg);
+end
