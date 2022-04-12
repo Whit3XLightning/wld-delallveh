085ccdab6f1819f5a12194aec45192bdd63bce36
Max W <87267714+FuriousF0xGG@users.noreply.github.com>
2022-04-12 22:15:44 +0100

    Updated config.lua due to my last chaneg
    IDK what to put here

--- config.lua
+++ config.lua
@@ -8,9 +8,4 @@ Config = {}
 Config = {
     commandName = "delallveh",
     -- This is the command that you will type into chat to execute the script.
-
-    restricCommand = true
-    -- Setting this to false will allow anyone in the server to use the command. 
-    -- If you set it to true you will need to add a ace perm to allow people to use it.
-    -- Such as add_ace [GROUP] command.[commandName] allow
 }
