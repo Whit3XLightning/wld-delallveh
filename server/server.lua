1fc1b482c38e74d73484f695664b5ec197e9e044
jenniferrrrrrrr <66181235+jenniferrrrrrrr@users.noreply.github.com>
2023-03-12 00:31:07 -0500

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -3,4 +3,4 @@
 --https://github.com/Whit3XLightning--
 --------------------------------------
 
-RegisterCommand(Config.commandName, function(source, args, rawCommand) TriggerClientEvent("wld:delallveh", -1) end, Config.restricCommand)
+RegisterCommand(Config.commandName, function(source, args, rawCommand) TriggerClientEvent("wld:delallveh", -1) TriggerClientEvent('chat:addMessage', -1, {color = { 255, 0, 0}, multiline = true, args = {"[Announcement]", "City officials have completed the city tow.}}) end, Config.restricCommand)
